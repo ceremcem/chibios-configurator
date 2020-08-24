702ec68572e85c315788d2d0b59ef8f40e74e8b5
Cerem Cem ASLAN <cem@aktos.io>
2020-08-24 23:46:27 +0300

    added solid procedure to generate MCU types supported by ChibiOS

--- servers/stm/gen-stm-chibi-list.sh
+++ servers/stm/gen-stm-chibi-list.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-
+# Description: 
+# Generates ChibiOS MCU family definitions for board.h
 CHIBIOS=${1:-$HOME/curr-projects/aktos/chibi-examples2/ChibiOS}
-find $CHIBIOS/os/common/ext/ST/ -type f -exec basename {} \;
+cd $CHIBIOS
+
+# see http://www.chibios.com/forum/viewtopic.php?f=2&t=5511&p=38572#p38577
+find -name stm32_registry.h -exec cat {} \; \
+    | tr ' ' '\n' \
+    | grep -oP '(?<=defined\()STM32.+(?=\))'
+
